48d561d6fbb6d111e97d5301a20e026f7139471e
BrendoBombaa <60665646+BrendoBombaa@users.noreply.github.com>
2020-02-04 14:31:31 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+	    
+	    System.out.println("Digite o nome do aluno");
+	    String nome = sc.nextLine();
+	    System.out.println("Informe a idade do aluno:");
+	    int idade = sc.nextInt();
+	    sc.close();
+	    
+	    System.out.println("Aluno " + nome + ", sua idade é " + idade + " anos.");
+	    }
+
+}
